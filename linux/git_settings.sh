8e2612ff572e4bb17d76ff88cd8bea1f2452cb85
Makoto Morita <mmakmo@gmail.com>
2017-02-15 00:31:40 +0900

    change git commit editor setting

--- linux/git_settings.sh
+++ linux/git_settings.sh
@@ -3,7 +3,7 @@
 # git config --global user.name "First-name Family-name"
 # git config --global user.email "username@example.com"
 
-git config --global core.editor 'vim -c "set fenc=utf-8"'
+git config --global core.editor 'nvim -c "set fenc=utf-8"'
 
 git config --global color.diff auto
 git config --global color.status auto
