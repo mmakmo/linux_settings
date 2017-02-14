397e81a3548177586d6dbff611402244f91014ef
Makoto Morita <mmakmo@gmail.com>
2017-02-15 00:24:22 +0900

    add nvim install settings

--- ubuntu/instal.sh
+++ ubuntu/instal.sh
@@ -9,5 +9,14 @@ aptitude install -y ssh tmux
 # install development tools
 aptitude install -y git bats vim
 
+# install neovim
+aptitude install -y software-properties-common
+add-apt-repository -y ppa:neovim-ppa/unstable
+aptitude update -y
+aptitude install -y neovim python3-dev python3-pip
+pip3 install -U pip3
+pip3 install --upgrade pip
+
 # cleanup
+echo "export XDG_CONFIG_HOME=~/.config" >> ~/.bashrc
 aptitude autoclean
