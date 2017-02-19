3a2cf9f83f7cc38bc85fbbd9a0cdd769c52d4ea6
Makoto Morita <mmakmo@gmail.com>
2017-02-19 16:29:36 +0900

    modify installing packages

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -25,15 +25,17 @@ echo $1 | sudo -S aptitude install -y software-properties-common
 echo $1 | sudo -S add-apt-repository -y ppa:neovim-ppa/unstable
 echo $1 | sudo -S aptitude update -y
 echo $1 | sudo -S aptitude install -y neovim
-echo "export XDG_CONFIG_HOME=~/.config" >> ~/.profile
+echo "export XDG_CONFIG_HOME=~/.config" >> $HOME/.profile
 
 # install python environments
-echo $1 | sudo -S aptitude install -y build-essential zlib1g-dev libbz2-dev \
-                                      libssl-dev libreadline-dev libncurses5-dev \
-                                      libsqlite3-dev libgdbm-dev libdb-dev libexpat-dev \
-                                      libpcap-dev liblzma-dev libpcre3-dev curl python-pip
+#echo $1 | sudo -S aptitude install -y build-essential zlib1g-dev libbz2-dev \
+#                                      libssl-dev libreadline-dev libncurses5-dev \
+#                                      libsqlite3-dev libgdbm-dev libdb-dev libexpat-dev \
+#                                      libpcap-dev liblzma-dev libpcre3-dev curl python-pip
+echo $1 | sudo -S aptitude install -y curl python-pip
 curl -kL https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
-echo '[[ -s $HOME/.pythonz/etc/bashrc ]] && source $HOME/.pythonz/etc/bashrc' >> $HOME/.bashrc
+echo '[[ -s $HOME/.pythonz/etc/bashrc ]] && source $HOME/.pythonz/etc/bashrc' >> $HOME/.profile
+exec $SHELL
 echo $1 | sudo -S aptitude install -y direnv
 echo $1 | sudo -S pip install --upgrade pip
 echo $1 | sudo -S pip install --upgrade virtualenv
