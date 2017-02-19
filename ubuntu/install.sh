9c8aa883a87b9832aa429a485d4cf7299e4db0a9
Makoto Morita <mmakmo@gmail.com>
2017-02-19 16:54:59 +0900

    add required package installation

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -28,7 +28,11 @@ echo $1 | sudo -S aptitude install -y neovim
 echo "export XDG_CONFIG_HOME=~/.config" >> $HOME/.profile
 
 # install python environments
-echo $1 | sudo -S aptitude install -y curl python-pip
+echo $1 | sudo -S aptitude install -y \
+                      build-essential zlib1g-dev libbz2-dev libssl-dev \
+                      libreadline-dev libncurses5-dev libsqlite3-dev \
+                      libgdbm-dev libdb-dev libexpat-dev libpcap-dev \
+                      liblzma-dev libpcre3-dev curl python-pip
 curl -kL https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
 echo '[[ -s $HOME/.pythonz/etc/bashrc ]] && source $HOME/.pythonz/etc/bashrc' >> $HOME/.bashrc
 exec $SHELL
