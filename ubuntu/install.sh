506ec7dea0ab9243792738c7d928d88391da6f0c
Makoto Morita <mmakmo@gmail.com>
2017-02-19 16:18:14 +0900

    change package management pyenv to direnv

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -23,34 +23,20 @@ echo $1 | sudo -S aptitude install -y git bats vim
 # install neovim
 echo $1 | sudo -S aptitude install -y software-properties-common
 echo $1 | sudo -S add-apt-repository -y ppa:neovim-ppa/unstable
-
-# install python environments
 echo $1 | sudo -S aptitude update -y
-echo $1 | sudo -S aptitude install -y neovim python-dev python-pip
-echo $1 | sudo -S pip install --upgrade pip
-echo $1 | sudo -S pip install --upgrade virtualenv
-echo $1 | sudo -S pip install --upgrade virtualenvwrapper
-
-git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-echo '
-export PYENV_ROOT=$HOME/.pyenv
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-source /usr/local/bin/virtualenvwrapper.sh' >> ~/.profile
-
-echo "# Virtualenvwrapper" >> ~/.profile
-echo "if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then" >> ~/.profile
-echo "    export WORKON_HOME='$HOME/.virtualenvs'" >> ~/.profile
-echo "    source /usr/local/bin/virtualenvwrapper.sh" >> ~/.profile
-echo "fi" >> ~/.profile
-
+echo $1 | sudo -S aptitude install -y neovim
 echo "export XDG_CONFIG_HOME=~/.config" >> ~/.profile
 
-
-#pyenv install anaconda3-4.3.0
-#pyenv global anaconda3-4.3.0
+# install python environments
+echo $1 | sudo -S aptitude install -y build-essential zlib1g-dev libbz2-dev \
+                                      libssl-dev libreadline-dev libncurses5-dev \
+                                      libsqlite3-dev libgdbm-dev libdb-dev libexpat-dev \
+                                      libpcap-dev liblzma-dev libpcre3-dev curl python-pip
+curl -kL https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
+echo '[[ -s $HOME/.pythonz/etc/bashrc ]] && source $HOME/.pythonz/etc/bashrc' >> $HOME/.bashrc
+echo $1 | sudo -S aptitude install -y direnv
+echo $1 | sudo -S pip install --upgrade pip
+echo $1 | sudo -S pip install --upgrade virtualenv
 
 # cleanup
 echo $1 | sudo -S aptitude autoclean
